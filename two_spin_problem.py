b520225d909fec57ac7d0d91acbc9583388248ad
Daniel O'Connor <52418744+dtoconnor@users.noreply.github.com>
2020-09-02 09:38:08 +0100

    Create two_spin_problem.py

---
+++ two_spin_problem.py
@@ -0,0 +1,31 @@
+from dwave.system import DWaveSampler
+from dwave.system.composites import EmbeddingComposite
+import dwave.inspector as dwi
+
+"""
+Follow the four scenarios listed in the Readme by changing the variables in this file
+
+qubit_1   qubit_2
+
+  O -------- O 
+        
+h_1, J_val, h_2
+ 
+"""
+
+qubit_1 = 0
+qubit_2 = 1
+
+h_1 = 0
+h_2 = 0
+
+J_val = -1
+
+h = {qubit_1: h_1, qubit_2: h_2}
+J = {(qubit_1, qubit_2): J_val}
+
+sampler = EmbeddingComposite(DWaveSampler(solver=dict(qpu=True)))
+
+response = sampler.sample_ising(h, J, num_reads=1000, num_spin_reversal_transforms=0)
+
+dwave.inspector.show(response)
